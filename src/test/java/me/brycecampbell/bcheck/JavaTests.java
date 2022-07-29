c80582ce5a9d5c9991c4df9888eabf3c273db068
Bryce Campbell <tonyhawk2100@gmail.com>
2022-07-29 14:41:50 -0700

    rename Java test file.

--- src/test/java/me/brycecampbell/bcheck/JavaTests.java
+++ src/test/java/me/brycecampbell/bcheck/JavaTests.java
@@ -2,7 +2,7 @@ package me.brycecampbell.bcheck;
 
 import org.junit.jupiter.api.Test;
 
-public class BCheckTests {
+public class JavaTests {
     @Test
     public void createEmptyTransaction() {
         Transaction transaction = new Transaction();
