7b08f9ad8c570516eb8374b773681263433d7636
Bryce Campbell <tonyhawk2100@gmail.com>
2022-07-29 14:40:57 -0700

    establish first test in Java.

--- src/test/java/me/brycecampbell/bcheck/BCheckTests.java
+++ src/test/java/me/brycecampbell/bcheck/BCheckTests.java
@@ -1,4 +1,12 @@
 package me.brycecampbell.bcheck;
 
+import org.junit.jupiter.api.Test;
+
 public class BCheckTests {
+    @Test
+    public void createEmptyTransaction() {
+        Transaction transaction = new Transaction();
+
+        assert transaction.getType() == TransactionType.Withdrawal;
+    }
 }
