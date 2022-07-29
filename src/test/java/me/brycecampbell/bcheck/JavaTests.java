57a415bfc067fe4f7c0151cdbb01dbb11c2b967e
Bryce Campbell <tonyhawk2100@gmail.com>
2022-07-29 15:23:14 -0700

    check that records can be imported in Java.

--- src/test/java/me/brycecampbell/bcheck/JavaTests.java
+++ src/test/java/me/brycecampbell/bcheck/JavaTests.java
@@ -2,6 +2,10 @@ package me.brycecampbell.bcheck;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JavaTests {
     @Test
     public void createEmptyTransaction() {
@@ -15,5 +19,27 @@ public class JavaTests {
         Record record = new Record();
 
         assert !record.getId().isEmpty();
+    }
+
+    @Test
+    public void retrieveRecordsFromFile() {
+        List<Record> expected_records = new ArrayList<>(Arrays.asList(
+                new Record("FF04C3DC-F0FE-472E-8737-0F4034C049F0",
+                        new Transaction("2021-07-08", 1260, null,
+                                "Sam Hill Credit Union", "Open Account", 500.0,
+                                TransactionType.Deposit, false)),
+                new Record("1422CBC6-7B0B-4584-B7AB-35167CC5647B",
+                        new Transaction("2021-07-08", 1260, null,
+                                "Fake Street Electronics", "Head set", 200.0,
+                                TransactionType.Withdrawal, false)),
+                new Record("BB22187E-0BD3-41E8-B3D8-8136BD700865",
+                        new Transaction("2021-07-08", 1260, null,
+                                "Velociraptor Entertainment", "Pay Day", 50000.0,
+                                TransactionType.Deposit, false))
+        ));
+
+        List<Record> records = Record.loadFromPath("/Users/bryce/Desktop/test.bcheck");
+
+        assert  records.size() == expected_records.size();
     }
 }
