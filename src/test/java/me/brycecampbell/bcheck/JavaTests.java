004d2afba9aca83092ae41400bee441409d7deb9
Bryce Campbell <tonyhawk2100@gmail.com>
2022-07-29 15:37:33 -0700

    finish implementing tests.

--- src/test/java/me/brycecampbell/bcheck/JavaTests.java
+++ src/test/java/me/brycecampbell/bcheck/JavaTests.java
@@ -42,4 +42,24 @@ public class JavaTests {
 
         assert  records.size() == expected_records.size();
     }
+
+    @Test
+    public void saveRecordsToFile() {
+        List<Record> records = new ArrayList<>(Arrays.asList(
+                new Record("FF04C3DC-F0FE-472E-8737-0F4034C049F0",
+                        new Transaction("2021-07-08", 1260, "Opening Balance",
+                                "Sam Hill Credit Union", "Open Account", 500.0,
+                                TransactionType.Deposit, true)),
+                new Record("1422CBC6-7B0B-4584-B7AB-35167CC5647B",
+                        new Transaction("2021-07-08", 1260, "Gifts",
+                                "Fake Street Electronics", "Head set", 200.0,
+                                TransactionType.Withdrawal, false)),
+                new Record("BB22187E-0BD3-41E8-B3D8-8136BD700865",
+                        new Transaction("2021-07-08", 1260, null,
+                                "Velociraptor Entertainment", "Pay Day", 50000.0,
+                                TransactionType.Deposit, false))
+        ));
+
+        saveToPath.saveToPath(records, "/Users/bryce/Desktop/test.bcheck");
+    }
 }
